cb795fdb0499bd3e2e05fe15072c200e131c6003
Tiago-O-Luz <tiagoluz64@gmail.com>
2024-06-30 12:29:12 -0300

    app updated

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ class Loan:
 
 class LibraryManager:
     def __init__(self):
-        # self.book_node = Node(1)
+        self.book_node = Node(1)
         self.clients: dict[str, Client] = {}
         self.loans: dict[tuple, Loan] = {}
     
@@ -40,6 +40,7 @@ class LibraryManager:
             return self.clients[ID].get_info()
         except KeyError:
             print("ERROR: Client not found")
+        return ()
 
     def add_client(self, ID: str, name: str, age: int):
         if ID not in self.clients:
@@ -49,14 +50,28 @@ class LibraryManager:
 
     def remove_client(self, ID):
         try:
+            client = self.clients[ID]
+            for loan in self.loans:
+                if loan.client.id == ID:
+                    print("ERROR: Client has pending loans")
+                    return
             del self.clients[ID]
         except KeyError:
             print("ERROR: Client not found")
 
     def loan_book(self, client_id: str, book_info: tuple):
+        try:
+            client = self.clients[client_id]
+        except KeyError:
+            print("ERROR: Client not found")
+            return
+            
         t = self.book_node.get(book_info)
+        if t == ():
+            print("ERROR: Book not found")
+            return
         book = Book(*t)
-        client = self.clients[client_id]
+
         self.loans[(client.ID, book.ID)] = Loan(book, client)
 
     def return_book(self, client_id: str, book_id: str):
@@ -76,10 +91,11 @@ class LibraryManager:
             print("ERROR: Couldn't delete loan")
     
     def add_book(self, ID: str, title: str, author: str, year: str, publisher: str):
+        # print(f"Creating book {ID, title, author, year, publisher}")
         book = Book(ID, title, author, year, publisher)
         t = book.get_info()
-        print(t)
-        self.book_node.write(t)
+        # print(t)
+        # self.book_node.write(t)
 
     def search_book(self, ID: str, title: str, author: str, year: str, publisher:str):
         book = Book(ID, title, author, year, publisher)
@@ -115,10 +131,11 @@ if __name__ == "__main__":
     run = True
     while run:
         request = input()
-        items = [x.split(" ") for x in request.split(" -")]
+        items = [x.split(" ", 1) for x in request.split(" -")]
         # print(items)
         cmd = items[0][0]
         args = {item[0]: item[1] for item in items[1:]}
+        # print(cmd)
         # print(args)
 
         if cmd == "addbk":
